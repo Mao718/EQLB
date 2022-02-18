9f9d30f3f095a4d9e46152a277dba06a1871bfd1
mao <zxc596666123@gmail.com>
2022-02-18 13:07:38 +0900

    sigma

--- embedding/embeddings.py
+++ embedding/embeddings.py
@@ -4,15 +4,15 @@ sys.path.append("..")
 from multiprocessing import Pool
 import multiprocessing
 import tqdm
-
+from model.sigma import Sigma
 from model.syncmap import SyncMap
 from preprocess.preprocess import *
 
-def work_package_for_mutiprocess(img:"(width,width,color)",repeat_time=10):
+def work_package_for_mutiprocess(img:"(width,width,color)",repeat_time=100):
     
     seq=fig2seq(img)
     #print(seq.shape)
-    model=SyncMap(seq.shape[1], 2)
+    model=Sigma(seq.shape[1], 10)
     for i in range(repeat_time):
         rep=model.input(seq)
     #rep=model.syncmap
